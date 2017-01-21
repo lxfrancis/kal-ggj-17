059507c0ac5daff43de6ef0c6b518a5d5ef7bbc7
Ken Wong <helloken@kenart.net>
2017-01-21 21:14:35 +1100

    properly push terrain tech pitchtracker

---
+++ kalggj17-Unity-project/Assets/Scripts/Panner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panner : MonoBehaviour {
+
+	public PitchTracker pitchTracker;
+	public Transform panner;
+	public float panSpeed = 1;
+	public Transform pitchMover;
+	public float heightScale = 3;
+
+	public float bottomLimit = 500;
+	public float topLimit = 1000;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		panner.position += new Vector3(panSpeed * Time.deltaTime, 0, 0);
+		float height = (pitchTracker.pitchValue - bottomLimit) / (topLimit - bottomLimit);
+		pitchMover.localPosition = new Vector3(0, Mathf.Clamp(pitchTracker.dbValue,-30,20)/30, 0);
+	}
+}
