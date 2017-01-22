e830bfc341ce7f9798a45c4aa3712dd2deaef924
Ken Wong <helloken@kenart.net>
2017-01-22 12:18:27 +1100

    add world config

---
+++ kalggj17-Unity-project/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour {
+
+	public WorldConfig[] worldConfigs;
+	public int currentConfig = 0;
+
+	public MeshRenderer terrainMR;
+	Material terrainMat;
+	public Light dirLight;
+
+	void Start()
+	{
+		terrainMat = terrainMR.material;
+		UpdateConfig();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		int prevConfig = currentConfig;
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			currentConfig ++;
+			if(currentConfig >= worldConfigs.Length)
+			{
+				currentConfig = 0;
+			}
+		}
+
+		if(currentConfig != prevConfig)
+		{
+			UpdateConfig();
+		}
+		
+	}
+
+	void UpdateConfig()
+	{
+		WorldConfig conf = worldConfigs[currentConfig];
+		terrainMat.SetTexture("_MainTex", conf.tex);
+		terrainMat.SetColor("_FogColor", conf.fogColor);
+		terrainMat.SetColor("_AmbientColor", conf.ambientColor);
+		dirLight.color = conf.dirLightColor;
+		Camera.main.backgroundColor = conf.skyColor;
+	}
+}
